9f0a227b8e848640b394bb2e3aa1328afbd17ad7
hoeventl <hoeventl@rose-hulman.edu>
2019-01-27 21:14:44 -0500

    DONE

--- src/m4_sequences.py
+++ src/m4_sequences.py
@@ -304,7 +304,7 @@ def practice_problem4c(points):
       :rtype: rg.Point | string
     """
     ###########################################################################
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #     The testing code is already written for you (above).
     #
     # IMPORTANT: This problem is your LOWEST PRIORITY for preparing
@@ -315,7 +315,14 @@ def practice_problem4c(points):
     #    DIFFICULTY:      9
     #    TIME ESTIMATE:   15 minutes.
     ###########################################################################
-
+    for k in range(len(points)):
+        if is_prime(points[k].x) is True and is_prime(points[k].y) is True:
+            x = points[k].x
+            y = points[k].y
+            points[k].x = y
+            points[k].y = x
+            return points[k]
+    return 'Not found'
 
 def run_test_practice_problem4d():
     """ Tests the    practice_problem4d    function. """
@@ -400,14 +407,20 @@ def practice_problem4d(sequence):
       :rtype: int
     """
     ###########################################################################
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #     The testing code is already written for you (above).
     ###########################################################################
     # DIFFICULTY AND TIME RATINGS (see top of this file for explanation)
     #    DIFFICULTY:      7
     #    TIME ESTIMATE:   15 minutes.
     ###########################################################################
-
+    total = 0
+    for k in range(len(sequence) - 1):
+        if is_prime(sequence[k]) is True:
+            if is_prime(sequence[k + 1]) is True:
+                if sequence[k] != sequence[k + 1]:
+                    total = total + sequence[k]
+    return total
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
