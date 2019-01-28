8ad78ee913cd987e64bac9fa93af62b36051105f
hoeventl <hoeventl@rose-hulman.edu>
2019-01-27 21:58:19 -0500

    DONE

--- src/m6_loops_within_loops_printing.py
+++ src/m6_loops_within_loops_printing.py
@@ -6,8 +6,9 @@ This problem provides practice at:
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Thomas Hoevener.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
+
 
 ###############################################################################
 # Students:
@@ -76,7 +77,7 @@ def shape(r):
     For purposes of "lining up", assume r is a single digit.
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     ###########################################################################
@@ -88,6 +89,17 @@ def shape(r):
     #    DIFFICULTY:      7
     #    TIME ESTIMATE:  15 minutes.
     # -------------------------------------------------------------------------
+    for k in range(r):
+        for i in range(k):
+            print(' ', end='')
+        for j in range(r - k, 0, -1):
+            print('+', end='')
+        print('!', end='')
+        for l in range(r - k, 0, -1):
+            print(l, end='')
+        for u in range(k):
+            print('-', end='')
+        print()
 
 
 # -----------------------------------------------------------------------------
